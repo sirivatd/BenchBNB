2860be39bba50c7291a9cdb553c2c64e71e1e826
sirivat <dsirivat17@gmail.com>
2018-10-08 16:53:20 -0700

    Create marker_manager

--- frontend/components/bench_map.jsx
+++ frontend/components/bench_map.jsx
@@ -1,16 +1,45 @@
 import React from "react";
+import MarkerManager from "./../util/marker_manager";
 
 class BenchMap extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  isEmpty(obj) {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) return false;
+    }
+    return true;
+  }
+
   componentDidMount() {
+    this.props.fetchBenches();
     const mapOptions = {
       center: { lat: 37.7758, lng: -122.435 },
       zoom: 13
     };
 
+    const { benches } = this.props;
+
     this.map = new google.maps.Map(this.mapNode, mapOptions);
+    this.MarkerManager = new MarkerManager(this.map);
   }
 
   render() {
+    const { benches } = this.props;
+
+    if (this.isEmpty(benches)) {
+    } else {
+      let benchArr = [];
+      benchArr = Object.keys(benches).map(key => ({
+        id: key,
+        description: benches[key].description,
+        lat: benches[key].lat,
+        lng: benches[key].lng
+      }));
+      console.log(benchArr);
+      this.MarkerManager.updateMarkers(benchArr);
+    }
     return <div id="map-container" ref={map => (this.mapNode = map)} />;
   }
 }
--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -5,6 +5,19 @@ export default class MarkerManager {
   }
 
   updateMarkers(benches) {
-    console.log("time to update");
+    console.log(`This are the benches in MarkerManager ${benches}`);
+    const benchesObj = {};
+    benches.forEach(bench => (benchesObj[bench.id] = bench));
+    benches.forEach(bench => this.createMarkerFromBench(bench));
+  }
+
+  createMarkerFromBench(bench) {
+    const position = new google.maps.LatLng(bench.lat, bench.lng);
+    const marker = new google.maps.Marker({
+      position,
+      map: this.map,
+      benchId: bench.id
+    });
+    this.markers[marker.benchId] = marker;
   }
 }
